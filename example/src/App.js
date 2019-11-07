81274d41eeabb61900b4e9a09aa54e2bbdabbbdd
Emmanuel Boutin <emmanuel.boutin@jabberbees.com>
2019-11-07 09:00:53 +0100

    Updated the example website

--- example/src/App.js
+++ example/src/App.js
@@ -1,15 +1,25 @@
 import React, { Component } from 'react';
-import { ab, AbTestVariant } from 'ab-react';
+import { ab, AbTestVariant } from '@jabberbees/ab-react';
 
 ab.configure()
   .addExperiment('hello-world')
   .addVariant("v1")
-  .addVariant("v2")
+  .addVariant("v2").weighing(2)
   .addVariant("v3")
   .addVariant("v4")
-  .done();
+  .addExperiment('zig-or-zag')
+  .addVariant("zig")
+  .addVariant("zag").forced()
+  .done()
+  .loadVariantMap("ab-map", window.localStorage)
+  .randomiseVariants()
+  .saveVariantMap("ab-map", window.localStorage);
 
 export default class App extends Component {
+  onResetVariants() {
+    ab.removeSavedVariantMap("ab-map", window.localStorage);
+    window.location.reload();
+  }
 
   render() {
     return (
@@ -26,6 +36,15 @@ export default class App extends Component {
         <AbTestVariant experiment='hello-world' variant='v4'>
           <p>Hello world! (v4)</p>
         </AbTestVariant>
+
+        <AbTestVariant experiment='zig-or-zag' variant='zig'>
+          <p>Zig</p>
+        </AbTestVariant>
+        <AbTestVariant experiment='zig-or-zag' variant='zag'>
+          <p>Definitely Zag</p>
+        </AbTestVariant>
+
+        <button onClick={this.onResetVariants}>Reset variants</button>
       </div>
     );
   }
